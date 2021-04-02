2237dd1ceafd1dd8b12754418faf577eff20fb3c
Timur <trew.tr2019@yandex.ru>
2021-04-02 15:11:01 +0300

    menu swiper without adaptive

--- js/app.js
+++ js/app.js
@@ -9,3 +9,13 @@ const mainFirstSwiper = new Swiper('.main-swiper', {
   // disableOnInteraction: false,
   // },
 });
+
+$('.menu__list__item').each(function () {
+  $(this).on('mouseover', (el) => {
+    $('.menu__list__item').removeClass('menu__list__item--active');
+    this.classList.add('menu__list__item--active');
+    if (el.target.dataset.image) {
+      $('.menu__image').attr('src', `${el.target.dataset.image}`);
+    }
+  });
+});
