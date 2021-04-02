c651b62a39f5119b7f8d5ac3d7fa6fe6a1297a6b
Timur <trew.tr2019@yandex.ru>
2021-04-02 19:15:00 +0300

    minified required set

---
+++ dist/js/app.js
@@ -0,0 +1,21 @@
+const mainFirstSwiper = new Swiper('.main-swiper', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.main-swiper__pagination',
+    clickable: true,
+  },
+  autoplay: {
+    disableOnInteraction: false,
+  },
+});
+
+$('.menu__list__item').each(function () {
+  $(this).on('mouseover', (el) => {
+    $('.menu__list__item').removeClass('menu__list__item--active');
+    this.classList.add('menu__list__item--active');
+    if (el.target.dataset.image) {
+      $('.menu__image').attr('src', `${el.target.dataset.image}`);
+    }
+  });
+});
